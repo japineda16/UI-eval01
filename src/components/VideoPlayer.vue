cc7786523afad801396e8a288e0cb49ca3c81b7c
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2024-11-26 11:41:22 -0400

    added video player

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="video-container">
+        <video ref="videoPlayer" class="video-js vjs-default-skin vjs-big-play-centered">
+            <source :src="videoPlayer">
+            <p class="vjs-no-js">
+                Para ver este video, por favor habilite JavaScript y considere actualizar
+                a un navegador web que soporte video HTML5
+            </p>
+        </video>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount, watch } from 'vue'
+import videojs from 'video.js'
+import 'video.js/dist/video-js.css'
+
+const props = defineProps({
+    url: {
+        type: String,
+        default: 'https://www.w3schools.com/html/mov_bbb.mp4',
+    }
+})
+
+const videoPlayer = ref(props.url)
+let player = null
+
+const initializePlayer = () => {
+    if (videoPlayer.value) {
+        player = videojs(videoPlayer.value, {
+            controls: true,
+            fluid: true,
+            playbackRates: [0.5, 1, 1.5, 2],
+            controlBar: {
+                children: [
+                    'playToggle',
+                    'volumePanel',
+                    'currentTimeDisplay',
+                    'timeDivider',
+                    'durationDisplay',
+                    'progressControl',
+                    'playbackRateMenuButton',
+                    'fullscreenToggle',
+                ],
+            },
+        })
+    }
+}
+
+// Inicializar el reproductor cuando el componente se monte
+onMounted(() => {
+    initializePlayer()
+})
+
+// Limpiar el reproductor cuando el componente se desmonte
+onBeforeUnmount(() => {
+    if (player) {
+        player.dispose()
+        player = null
+    }
+})
+
+// Observar cambios en la URL del video
+watch(() => props.url, (newUrl) => {
+    if (player) {
+        player.src({ src: newUrl })
+    }
+})
+</script>
+
+<style scoped>
+.video-container {
+    width: 100%;
+    max-width: 800px;
+    margin: 0 auto;
+}
+
+:deep(.video-js) {
+    width: 100%;
+    height: 0;
+    padding-top: 56.25%;
+    /* Aspect ratio 16:9 */
+}
+
+:deep(.video-js.vjs-fluid) {
+    padding-top: 56.25%;
+    width: 100%;
+    max-width: 100%;
+    height: auto;
+}
+
+:deep(.video-js .vjs-big-play-button) {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
